2f38a33602770ec8db2d156acbd67ad0256faa46
Herman Gudjonson <herman.gudjonson@gmail.com>
2024-02-29 11:36:45 -0500

    update batch time
    adding matmul time for comparison

--- image_basics/tasks/gpu_init.py
+++ image_basics/tasks/gpu_init.py
@@ -40,8 +40,8 @@ def easy_pets_recipe():
     return hparams
 
 
-def batch_time(batch_sizes=None, output_dir=None, device=None):
-    """Compare forward pass and train() timing across batch sizes.
+def batch_time(batch_sizes=None, subset_size=None, output_dir=None, device=None):
+    """Compare matmul, forward pass and train() timing across batch sizes.
 
     Parameters
     ----------
@@ -65,6 +65,9 @@ def batch_time(batch_sizes=None, output_dir=None, device=None):
     for b in batch_sizes:
         hparams["data_params"]["train_batch_size"] = b
         hparams["data_params"]["val_batch_size"] = b
+        if subset_size is not None:
+            hparams["data_params"]["train_subset"] = subset_size
+            hparams["data_params"]["val_subset"] = subset_size
         hparams["device"] = device
 
         trainer = train.create_trainer(**hparams)
@@ -72,17 +75,33 @@ def batch_time(batch_sizes=None, output_dir=None, device=None):
         device = utils.get_device(device)
         print(f"using device {device}")
 
+        # matmul
+        A = torch.randn(16, 3 * 244 * 244, device=device)  # ~3M params
+        b = torch.randn(3 * 244 * 244, b, device=device)
+        timer = Timer(
+            "A.matmul(b)",
+            globals={"A": A, "b": b},
+            description=f"batch {b}",
+            sub_label="matmul",
+        )
+        timer_result = timer.blocked_autorange(min_run_time=1)
+        matmul_time = timer_result.median
+        timer_list.append(timer_result)
+
+        # forward pass
         X_rand = torch.randn(b, 3, 244, 244, device=device)
         trainer.model.to(device)
         timer = Timer(
             "trainer.model(X_rand)",
             globals={"trainer": trainer, "X_rand": X_rand},
             description=f"batch {b}",
+            sub_label="forward pass",
         )
         timer_result = timer.blocked_autorange(min_run_time=1)
         forward_time = timer_result.median
         timer_list.append(timer_result)
 
+        # trainer.train()
         if device.type == "gpu":
             torch.cuda.synchronize()
         train_start = time.time()
@@ -94,11 +113,17 @@ def batch_time(batch_sizes=None, output_dir=None, device=None):
 
         results[b] = dict(
             **{
+                "matmul_time": matmul_time,
+                "matmul_time_per_image": matmul_time / b,
+                "matmul_images_per_s": b / matmul_time,
                 "forward_time": forward_time,
                 "forward_time_per_image": forward_time / b,
+                "forward_images_per_s": b / forward_time,
                 "train_time": train_time,
                 "train_step_time": train_time
                 / (trainer.num_epochs * len(trainer.train_dl)),
+                "train_images_per_s": (trainer.num_epochs * len(trainer.train_dl) * b)
+                / train_time,
             },
             **trainer.best_metrics,
         )
